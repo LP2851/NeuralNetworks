ee9df04e46846925995a74a57f2d3a242e5051db
LucasPhillips28 <lucas.phillips28@hotmail.com>
2022-09-12 16:56:07 +0100

    Removed Mnist from DataPoint.java and made it abstract

--- src/data/DataPoint.java
+++ src/data/DataPoint.java
@@ -1,16 +1,14 @@
 package data;
 
-import static mnist.MnistReader.MNIST_IMAGE_TOTAL_SIZE;
-import static mnist.MnistReader.MNIST_IMAGE_WIDTH_HEIGHT;
 
 /**
- * @version 0.1
  * @author lucas
+ * @version 0.2
  */
-public class DataPoint {
+public abstract class DataPoint<E> {
 
     public double[] inputs, expectedOutputs;
-    public int label;
+    public final E label;
 
     /**
      *
@@ -20,38 +18,18 @@ public class DataPoint {
     public DataPoint(double[] inputs, double[] expectedOutputs) {
         this.inputs = inputs;
         this.expectedOutputs = expectedOutputs;
+        label = null;
     }
 
     /**
-     *
-     * @param inputs Values that are for the input of the neural network
+     * @param inputs          Values that are for the input of the neural network
      * @param expectedOutputs Expected outputs for the neural network
-     * @param label Label that has been given to the data point
+     * @param label           Label that has been given to the data point
      */
-    public DataPoint(double[] inputs, double[] expectedOutputs, int label) {
+    public DataPoint(double[] inputs, double[] expectedOutputs, E label) {
         this.inputs = inputs;
         this.expectedOutputs = expectedOutputs;
         this.label = label;
     }
 
-    /**
-     * Creates a data point from a Mnist input
-     * @param inputImage Input read from Mnist
-     * @param expectedOutput Label given to the data point
-     * @return The newly created data point from the Mnist data
-     */
-    public static DataPoint createMnistDataPoint(double[][] inputImage, int expectedOutput) {
-        double[] inputs = new double[MNIST_IMAGE_TOTAL_SIZE];
-
-        for (int x = 0; x < MNIST_IMAGE_WIDTH_HEIGHT; x++) {
-            for (int y = 0; y < MNIST_IMAGE_WIDTH_HEIGHT; y++) {
-                inputs[x * MNIST_IMAGE_WIDTH_HEIGHT + y] = inputImage[x][y] / 255d;
-            }
-        }
-
-        double[] out = new double[10];
-        out[expectedOutput] = 1d;
-
-        return new DataPoint(inputs, out, expectedOutput);
-    }
 }
